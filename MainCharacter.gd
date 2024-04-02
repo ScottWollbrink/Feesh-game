982e5db16900289f5040b91505fa9b890b78ecb3
Scott Wollbrink <sawollbrink@student.fullsail.edu>
2024-04-02 00:17:14 -0500

    fixer turtle issue

--- MainCharacter.gd
+++ MainCharacter.gd
@@ -17,14 +17,10 @@ func move_character():
 	if Input.is_action_just_pressed("move_right"):
 		isRight = true
 		isLeft = false
-		_animationPlayer.play("move")
+		
 	elif Input.is_action_just_pressed("move_left"):
 		isLeft = true
 		isRight = false
-		_animationPlayer.play("move")
-	else:
-		# Stop animation only when no movement keys are pressed
-		_animationPlayer.stop()
 
 	# Flip character horizontally based on direction
 	if isLeft:
@@ -33,4 +29,8 @@ func move_character():
 		_animationPlayer.flip_h = false
 
 	velocity = direction * _playerVelocity
+	if velocity != Vector2.ZERO:
+		_animationPlayer.play("move")
+	else:
+		_animationPlayer.stop()
 	move_and_slide()
--- Turtle.gd
+++ Turtle.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/MainCharacter")
+@onready var _animationPlayer = $TurtleAnimation
 
 func _physics_process(delta):
+	print(player.global_position)
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 150
+	var isLeft = velocity.x < 0
+	var isRight = velocity.x > 0
+	
+	if isRight:
+		_animationPlayer.flip_h = false
+	elif isLeft:
+		_animationPlayer.flip_h = true
+	
 	move_and_slide()
